a00c7f17df0684f196818e3fee527d318ac2bc7a
Yash <yashshetty40@gmail.com>
2020-12-29 12:06:10 +0530

    Initial Commit

---
+++ precache-manifest.609b0b9add03f5aa16457dea80f1f0bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce13a5a1088e394018017537574954ce",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "23b5b02e1e0df07f2f75",
+    "url": "/portfolio/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "95ea5c2ae10eb35bb877",
+    "url": "/portfolio/static/js/2.448dd0be.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/portfolio/static/js/2.448dd0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b5b02e1e0df07f2f75",
+    "url": "/portfolio/static/js/main.04d012b5.chunk.js"
+  },
+  {
+    "revision": "9c82abddd2577f6b4fb4",
+    "url": "/portfolio/static/js/runtime-main.e90eb3be.js"
+  },
+  {
+    "revision": "f986455d53fd2b484ffe46a5ff4d1504",
+    "url": "/portfolio/static/media/resume.f986455d.pdf"
+  }
+]);
